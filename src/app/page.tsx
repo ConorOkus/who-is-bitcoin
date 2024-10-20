32549c2d70e1d8db17a71c7bebf6600f478ae9b5
Abhijay007 <Abhijay007j@gmail.com>
2024-10-20 17:06:54 +0530

    Chore: Added Sticker set component
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Box } from "@chakra-ui/react";
-import { ColorProvider } from "../providers/ColorProvider";
+import { ColorProvider } from "@/providers/ColorProvider";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/HeroSection/HeroSection";
-import AssetsSection from "../components/AssetsSection/AssetsSection";
-import GifsSection from "../components/GifsSection/GifsSection";
-
+import AssetsSection from "@/components/AssetsSection/AssetsSection";
+import StickerSet from "@/components/StrickerSet/StickerSet";
+import GifsSection from "@/components/GifsSection/GifsSection";
 
 const Home: React.FC = () => {
   return (
@@ -23,6 +23,7 @@ const Home: React.FC = () => {
       >
         <AssetsSection />
       </Box>
+      <StickerSet />
       <Box
         px={{
           base: 6,
---
+++ src/components/StrickerSet/StickerSet.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Box, Heading, Text, Flex, Image, VStack, HStack } from '@chakra-ui/react';
+import CustomButton from '../CustomButton/CustomButton';
+
+const StickerSet = () => {
+  return (
+    <Box bg="#FFF1E5" w="full" overflow="hidden" px={{
+      base: 6,
+      lg: "100px",
+    }} >
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        mx="auto"
+        align="center"
+        justify="space-between"
+        overflow="hidden"
+      >
+        <VStack
+          align={{ base: 'center', md: 'flex-start' }}
+          spacing={4}
+          p={8}
+          mb={{ base: 8, md: 0 }}
+          w={{ base: 'full', md: '40%' }}
+        >
+          <Heading as="h1" size="2xl" color="navy.700" textAlign={{ base: 'center', md: 'left' }}>
+            Make chats stickier
+          </Heading>
+          <Text fontSize="lg" color="gray.600" textAlign={{ base: 'center', md: 'left' }}>
+            Orange up your Signal and Telegram chats with our Here Comes Bitcoin sticker set bundles, featuring all your favorite HCB Bitcoins
+          </Text>
+
+          <HStack spacing={10} mt={6} w={{ base: 'full', md: 'auto' }} justifyContent={{ base: 'center', md: 'flex-start' }}>
+            <CustomButton bg="#FFEFA6" color="#0C5156" fontSize="24px" padding="1.7rem 2rem">
+              Telegram Stickers
+            </CustomButton>
+            <CustomButton bg="#FFEFA6" color="#0C5156" fontSize="24px" padding="1.7rem 2rem">
+              Signal Stickers
+            </CustomButton>
+          </HStack>
+        </VStack>
+
+        <Flex
+          w={{ base: 'full', md: '55%' }}
+          position="relative"
+          justify="center"
+          align="center"
+          mt={{ base: 4, md: 0 }}
+          mb={{ base: 4, md: 0 }}
+          px={{ base: 0, md: 4 }}
+        >
+          <Box
+            position="relative"
+            w={{ base: 'full', md: 'auto' }}
+            maxW={{ base: '200px', md: '250px' }}
+            mr={{ base: 0, md: '5%' }}
+            mt={{ base: 0, md: '100px' }}
+          >
+            <Image
+              src="/assets/StrickerSetImages/Mobile-preview-One.png"
+              alt="Telegram chat preview"
+              w="100%"
+              h="auto"
+              objectFit="contain"
+              borderRadius="xl"
+              border="2px solid transparent"
+            />
+          </Box>
+          <Box
+            position="relative"
+            w={{ base: 'full', md: 'auto' }}
+            maxW={{ base: '200px', md: '250px' }}
+            ml={{ base: 0, md: '10%' }}
+            mt={{ base: 0, md: '-100px' }}
+          >
+            <Image
+              src="/assets/StrickerSetImages/Mobile-preview-Two.png"
+              alt="Signal chat preview"
+              w="100%"
+              h="auto"
+              objectFit="contain"
+              borderRadius="xl"
+              border="2px solid transparent"
+            />
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default StickerSet;
