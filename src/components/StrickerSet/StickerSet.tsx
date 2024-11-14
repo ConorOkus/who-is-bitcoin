e29423faea50f3594bd57f52ae973887fd174435
Abhijay007 <Abhijay007j@gmail.com>
2024-11-14 07:59:04 +0530

    (fix): Fixed StrickerSet section alignment in the mobile view
    Signed-off-by: Abhijay007 <Abhijay007j@gmail.com>

--- src/components/StrickerSet/StickerSet.tsx
+++ src/components/StrickerSet/StickerSet.tsx
@@ -7,16 +7,18 @@ const StickerSet = () => {
     <Box bg="#FFF1E5" w="full" overflow="hidden" px={{
       base: 6,
       lg: "100px",
-    }} >
+    }}>
       <Flex
         direction={{ base: 'column', md: 'row' }}
         mx="auto"
         align="center"
-        justify="space-between"
+        justify="center" 
         overflow="hidden"
+        textAlign={{ base: 'center', md: 'left' }} 
+        py={{ base: 8, md: 0 }} 
       >
         <VStack
-          align={{ base: 'center', md: 'flex-start' }}
+          align="center" 
           spacing={4}
           p={8}
           mb={{ base: 8, md: 0 }}
@@ -24,18 +26,40 @@ const StickerSet = () => {
         >
           <Heading
             fontSize={{ base: "4xl", md: "5xl", lg: "6xl" }}
-            fontWeight="medium">
+            fontWeight="medium"
+            textAlign="center"
+          >
             Make Chats Stickier
           </Heading>
-          <Text fontSize="lg" color="gray.600" textAlign={{ base: 'center', md: 'left' }}>
+          <Text fontSize="lg" color="gray.600">
             Orange up your Signal and Telegram chats with our Here Comes Bitcoin sticker set bundles, featuring all your favorite HCB Bitcoins
           </Text>
 
-          <Stack direction={{ base: 'column', md: 'row' }} spacing={{ base: 8, md: 6 }} mt={6} w={{ base: 'full', md: 'full' }} justifyContent={{ base: 'center', md: 'center' }}>
-            <CustomButton bg="#FFEFA6" href="https://t.me/addstickers/herecomesbitcoin" color="#0C5156" fontSize="24px" padding="1.7rem 2rem">
+          <Stack
+            direction={{ base: 'column', md: 'row' }} 
+            spacing={{ base: 8, md: 6 }} 
+            mt={6} 
+            w={{ base: 'full', md: 'full' }}
+            justifyContent="center"
+          >
+            <CustomButton
+              bg="#FFEFA6"
+              href="https://t.me/addstickers/herecomesbitcoin"
+              color="#0C5156"
+              fontSize="24px"
+              padding="1.7rem 2rem"
+              w="full" 
+            >
               Telegram Stickers
             </CustomButton>
-            <CustomButton bg="#FFEFA6" href="https://signal.art/addstickers/#pack_id=961a70b4646d38f98ebca674c2792262&pack_key=f6a446830125c12ba25bfe4e6138288b0babe1d54f3990465338cdd5a80fd7e9" color="#0C5156" fontSize="24px" padding="1.7rem 2rem">
+            <CustomButton
+              bg="#FFEFA6"
+              href="https://signal.art/addstickers/#pack_id=961a70b4646d38f98ebca674c2792262&pack_key=f6a446830125c12ba25bfe4e6138288b0babe1d54f3990465338cdd5a80fd7e9"
+              color="#0C5156"
+              fontSize="24px"
+              padding="1.7rem 2rem"
+              w="full" 
+            >
               Signal Stickers
             </CustomButton>
           </Stack>
@@ -44,7 +68,7 @@ const StickerSet = () => {
         <Flex
           w={{ base: 'full', md: '55%' }}
           position="relative"
-          justify="center"
+          justify="center" 
           align="center"
           mt={{ base: 4, md: 0 }}
           mb={{ base: 4, md: 0 }}
